586b32f2ea57548caddeeb7e255b25379607f0c1
AdityaGangadhare <35621964+AdityaGangadhare@users.noreply.github.com>
2018-07-31 21:13:10 +0530

    Version 0

---
+++ Fusion_v0.py
@@ -0,0 +1,265 @@
+import utm
+import numpy as np
+#%matplotlib inline
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from sympy import Symbol, symbols, Matrix, sin, cos
+from sympy import init_printing
+from sympy.utilities.codegen import codegen
+init_printing(use_latex=True)
+
+
+def convert_date(date_bytes):
+    return mdates.strpdate2num('%y%m%d')(date_bytes.decode('ascii'))
+    
+numstates=6 # States
+
+dt = 1.0/50.0 # Sample Rate of the Measurements is 50Hz
+dtGPS=1.0/10.0 # Sample Rate of GPS is 10Hz
+
+# Dynaic Matrix - This formulas calculate how the state is evolving from one to the next time step
+vs, psis, dpsis, dts, xs, ys, lats, lons, axs = symbols('v \psi \dot\psi T x y lat lon a')
+
+gs = Matrix([[xs+(vs/dpsis)*(sin(psis+dpsis*dts)-sin(psis))],
+             [ys+(vs/dpsis)*(-cos(psis+dpsis*dts)+cos(psis))],
+             [psis+dpsis*dts],
+             [axs*dts + vs],
+             [dpsis],
+             [axs]])
+state = Matrix([xs,ys,psis,vs,dpsis,axs])
+
+#Calculate the Jacobian of the Dynamic Matrix with respect to the state vector
+
+gs.jacobian(state)
+
+# Initial Uncertainty P
+P = np.diag([1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0])
+
+# Process Noise Covariance Matrix Q
+
+sGPS     = 0.5*8.8*dt**2  # assume 8.8m/s2 as maximum acceleration, forcing the vehicle
+sCourse  = 0.1*dt # assume 0.1rad/s as maximum turn rate for the vehicle
+sVelocity= 8.8*dt # assume 8.8m/s2 as maximum acceleration, forcing the vehicle
+sYaw     = 1.0*dt # assume 1.0rad/s2 as the maximum turn rate acceleration for the vehicle
+sAccel   = 0.5
+
+Q = np.diag([sGPS**2, sGPS**2, sCourse**2, sVelocity**2, sYaw**2, sAccel**2])
+
+# Real Measurements
+
+datafile = '2014-03-26-000-Data.csv'
+
+date, \
+time, \
+millis, \
+ax, \
+ay, \
+az, \
+rollrate, \
+pitchrate, \
+yawrate, \
+roll, \
+pitch, \
+yaw, \
+speed, \
+course, \
+latitude, \
+longitude, \
+altitude, \
+pdop, \
+hdop, \
+vdop, \
+epe, \
+fix, \
+satellites_view, \
+satellites_used, \
+temp = np.loadtxt(datafile, delimiter=',', unpack=True, skiprows=1)
+
+print('Read \'%s\' successfully.' % datafile)
+
+# A course of 0° means the Car is traveling north bound
+# and 90° means it is traveling east bound.
+# In the Calculation following, East is Zero and North is 90°
+# We need an offset.
+course =(-course+90.0)
+
+# Measurement Function H
+
+hs = Matrix([[xs], [ys], [vs], [dpsis], [axs]])
+
+# If no GPS measurement is available, simply set the corresponding values in $J_h$ to zero.
+JHs=hs.jacobian(state)
+
+# Measurement Noise Covariance
+
+varGPS = 5.0  # Standard Deviation of GPS Measurement# Standa 
+varspeed = 3.0 # Variance of the speed measurement
+varyaw = 0.1 # Variance of the yawrate measurement
+varacc = 1.0 # Variance of the longitudinal Acceleration
+R = np.diag([varGPS**2, varGPS**2, varspeed**2, varyaw**2, varacc**2])
+
+#print(R, R.shape)
+
+# Identity Matrix
+
+I = np.eye(numstates)
+#print(I, I.shape)
+
+# Approx. Lat/Lon to Meters to check Location
+
+RadiusEarth = 6378388.0 # m
+arc= 2.0*np.pi*(RadiusEarth+altitude)/360.0 # m/°
+
+dx = arc * np.cos(latitude*np.pi/180.0) * np.hstack((0.0, np.diff(longitude))) # in m
+dy = arc * np.hstack((0.0, np.diff(latitude))) # in m
+
+mx = np.cumsum(dx)
+my = np.cumsum(dy)
+
+ds = np.sqrt(dx**2+dy**2)
+
+GPS=(ds!=0.0).astype('bool') # GPS Trigger for Kalman Filter
+
+# Initial State
+
+x = np.matrix([[mx[0], my[0], course[0]/180.0*np.pi, speed[0]/3.6+0.001, yawrate[0]/180.0*np.pi, ax[0]]]).T
+print(x, x.shape)
+
+U=float(np.cos(x[2])*x[3])
+V=float(np.sin(x[2])*x[3])
+
+plt.quiver(x[0], x[1], U, V)
+plt.scatter(float(x[0]), float(x[1]), s=100)
+plt.title('Initial Location')
+plt.axis('equal')
+
+
+measurements = np.vstack((mx, my, speed/3.6, yawrate/180.0*np.pi, ax))
+# Lenth of the measurement
+m = measurements.shape[1]
+print(measurements.shape)
+
+# Preallocation for Plotting
+x0 = []
+x1 = []
+x2 = []
+x3 = []
+x4 = []
+x5 = []
+x6 = []
+Zx = []
+Zy = []
+Px = []
+Py = []
+Pdx= []
+Pdy= []
+Pddx=[]
+Pddy=[]
+Pdv =[]
+Kx = []
+Ky = []
+Kdx= []
+Kdy= []
+Kddx=[]
+Kdv= []
+dstate=[]
+
+
+for  filterstep  in range(m):
+
+    # Time Update (Prediction)
+    # ========================
+    # Project the state ahead
+    # see "Dynamic Matrix"
+    if np.abs(yawrate[filterstep])<0.0001: # Driving straight
+        x[0] = x[0] + x[3]*dt * np.cos(x[2])
+        x[1] = x[1] + x[3]*dt * np.sin(x[2])
+        x[2] = x[2]
+        x[3] = x[3] + x[5]*dt
+        x[4] = 0.0000001 # avoid numerical issues in Jacobians
+        x[5] = x[5]
+        dstate.append(0)
+    else: # otherwise
+        x[0] = x[0] + (x[3]/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2]))
+        x[1] = x[1] + (x[3]/x[4]) * (-np.cos(x[4]*dt+x[2])+ np.cos(x[2]))
+        x[2] = (x[2] + x[4]*dt + np.pi) % (2.0*np.pi) - np.pi
+        x[3] = x[3] + x[5]*dt
+        x[4] = x[4] # Constant Turn Rate
+        x[5] = x[5] # Constant Acceleration
+        dstate.append(1)
+    
+    # Calculate the Jacobian of the Dynamic Matrix A
+    # see "Calculate the Jacobian of the Dynamic Matrix with respect to the state vector"
+    a13 = float((x[3]/x[4]) * (np.cos(x[4]*dt+x[2]) - np.cos(x[2])))
+    a14 = float((1.0/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
+    a15 = float((dt*x[3]/x[4])*np.cos(x[4]*dt+x[2]) - (x[3]/x[4]**2)*(np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
+    a23 = float((x[3]/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
+    a24 = float((1.0/x[4]) * (-np.cos(x[4]*dt+x[2]) + np.cos(x[2])))
+    a25 = float((dt*x[3]/x[4])*np.sin(x[4]*dt+x[2]) - (x[3]/x[4]**2)*(-np.cos(x[4]*dt+x[2]) + np.cos(x[2])))
+    JA = np.matrix([[1.0, 0.0, a13, a14, a15, 0.0],
+                    [0.0, 1.0, a23, a24, a25, 0.0],
+                    [0.0, 0.0, 1.0, 0.0, dt, 0.0],
+                    [0.0, 0.0, 0.0, 1.0, 0.0, dt],
+                    [0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
+    
+    
+    # Project the error covariance ahead
+    P = JA*P*JA.T + Q
+    
+    # Measurement Update (Correction)
+    # ===============================
+    # Measurement Function
+    hx = np.matrix([[float(x[0])],
+                    [float(x[1])],
+                    [float(x[3])],
+                    [float(x[4])],
+                    [float(x[5])]])
+    
+
+    if GPS[filterstep]: # with 10Hz, every 5th step
+        JH = np.matrix([[1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
+    else: # every other step
+        JH = np.matrix([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])        
+    
+    S = JH*P*JH.T + R
+    K = (P*JH.T) * np.linalg.inv(S)
+
+    # Update the estimate via
+    Z = measurements[:,filterstep].reshape(JH.shape[0],1)
+    y = Z - (hx)                         # Innovation or Residual
+    x = x + (K*y)
+
+    # Update the error covariance
+    P = (I - (K*JH))*P
+
+    # Save states for Plotting
+    x0.append(float(x[0]))
+    x1.append(float(x[1]))
+    x2.append(float(x[2]))
+    x3.append(float(x[3]))
+    x4.append(float(x[4]))
+    x5.append(float(x[5]))
+    Zx.append(float(Z[0]))
+    Zy.append(float(Z[1]))    
+    Px.append(float(P[0,0]))
+    Py.append(float(P[1,1]))
+    Pdx.append(float(P[2,2]))
+    Pdy.append(float(P[3,3]))
+    Pddx.append(float(P[4,4]))
+    Pdv.append(float(P[5,5]))
+    Kx.append(float(K[0,0]))
+    Ky.append(float(K[1,0]))
+    Kdx.append(float(K[2,0]))
+    Kdy.append(float(K[3,0]))
+    Kddx.append(float(K[4,0]))
+    Kdv.append(float(K[5,0]))
